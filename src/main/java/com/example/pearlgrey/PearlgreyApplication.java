b25c133bf97aee985ec9e61a44c6da3c931ad522
unknown <salamiabdulbasit123@gmail.com>
2025-11-18 09:48:42 +0100

    Springboot file for Backend

---
+++ src/main/java/com/example/pearlgrey/PearlgreyApplication.java
@@ -0,0 +1,13 @@
+package com.example.pearlgrey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PearlgreyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PearlgreyApplication.class, args);
+	}
+
+}
